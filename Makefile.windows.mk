cb0f87f971f10c422ce363bad70885feba4f3c4d
Valera V.Kharseko <vharseko@office.xxlive.ru>
2018-07-09 20:09:40 +0300

    CC32 := $(cygpath -u "${CC32}")

--- Makefile.windows.mk
+++ Makefile.windows.mk
@@ -70,8 +70,8 @@ else
  CFLAGS += /MT
 endif
 
-CC32 :=cmd /c "$(CC32)"
-dLINK32 :=link.exe
+CC32 := $(cygpath -u "${CC32}")
+LINK32 :=$(cygpath -u "${LINK32}")
 	
 libopenam: $(OUT_OBJS)
 	@$(ECHO) "[*** Creating "$@" shared library ***]"
