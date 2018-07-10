c8eb9c5fae3190ddeb929d54e8a53dbd514835c0
Valera V.Kharseko <vharseko@office.xxlive.ru>
2018-07-10 18:11:01 +0300

    i

--- Makefile.windows.mk
+++ Makefile.windows.mk
@@ -79,7 +79,7 @@ endif
 CC32:=C:\Program\ Files\ \(x86\)\Microsoft\ Visual\ Studio\ 14.0\VC\bin\cl.exe
 #LINK32:=$(shell cygpath "$(LINK32)")
 LINK32 :=C:\Program\ Files\ \(x86\)\Microsoft\ Visual\ Studio\ 14.0\VC\bin\link.exe
-LIB32_VC:=C:\Program\ Files\ \(x86\)\Microsoft\ Visual\ Studio\ 14.0\VC
+LIB32_VC:=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
 
 libopenam: $(OUT_OBJS)
 	@$(ECHO) "[*** Creating "$@" shared library ***]"
@@ -130,8 +130,8 @@ iis32: $(OUT_OBJS) $(IIS_OUT_OBJS)
 	$(SED) -e "s$(SUB)_FILE_NAME_$(SUB)mod_iis_openam_32.dll$(SUB)g" \
 	       -e "s$(SUB)_FILE_TYPE_$(SUB)VFT_DLL$(SUB)g"  source/version.rc.template >> $(OBJDIR)/version.rc
 	$(RC)  /l 0x0409 /nologo /fo $(OBJDIR)$(PS)version.res $(OBJDIR)$(PS)version.rc
-	$(LINK32) $(SHARED) /LIBPATH:\"$(LIB32_SDK)\x86\" 
-	#/LIBPATH:"$(LIB32_VC)\LIB" $(LDFLAGS) $(OUT_OBJS) $(IIS_OUT_OBJS) $(OBJDIR)/version.res /OUT:build/mod_iis_openam_32.dll /PDB:build/mod_iis_openam_32.pdb $(LIBS) /EXPORT:RegisterModule oleaut32.lib
+	$(LINK32) $(SHARED) /LIBPATH:\"$(LIB32_SDK)\x86\" /LIBPATH:\"$(LIB32_VC)\LIB\" 
+	#$(LDFLAGS) $(OUT_OBJS) $(IIS_OUT_OBJS) $(OBJDIR)/version.res /OUT:build/mod_iis_openam_32.dll /PDB:build/mod_iis_openam_32.pdb $(LIBS) /EXPORT:RegisterModule oleaut32.lib
 	$(CP) $(OBJDIR)$(PS)mod_iis_openam_32.dll $(OBJDIR)$(PS)dist
 	$(CP) $(OBJDIR)$(PS)mod_iis_openam_32.pdb $(OBJDIR)$(PS)dist
 
